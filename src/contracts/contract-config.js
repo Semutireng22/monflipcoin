a0dbf28c4bd5f42b919a39e57215feebf75a871d
Semutireng22 <mbohlahcok22@gmail.com>
2025-07-07 00:01:33 +0700

    v1.0.3 UI

--- src/contracts/contract-config.js
+++
@@ -1,3 +0,0 @@
-// src/contracts/contract-config.js
-export const contractAddress = "0x6dfE4770f02688EBCA73B9bC893A14254A8a1340";
-export const explorerUrl = "https://testnet.monadexplorer.com"; // Sesuaikan jika explorer URL berbeda
