f5349e45c3ddee53d13707af8c50b9d5e97e52be
BB22 <79566123+Semutireng22@users.noreply.github.com>
2025-08-29 21:53:23 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 // Alamat smart contract game Anda
-export const contractAddress = "0x664e248c39cd70Fa333E9b2544beEd6A7a2De09b";
+export const contractAddress = "0xF696812E390527bc15fc3f7661A83A3a02E22920";
 
 // URL dasar dari explorer Monad Testnet
 export const explorerUrl = "https://testnet.monadexplorer.com";
